99362cff1c18b1d53a7d660e2a76962df832c9d9
VolodyaHovhannisyan <volodyahovhannisyan000@gmail.com>
2022-08-04 20:43:01 +0400

    Added 2 sections

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./*.html'],
+  plugins: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'homesection': "url('/assets/bg-home-section.png')"
+      }
+    },
+    fontFamily: {
+      'montserrat': ['Nunito', 'sans-serif']
+    }
+  }
+}
