8c885b33cdaafb02e2f29c987832228057fd00f6
VolodyaHovhannisyan <volodyahovhannisyan000@gmail.com>
2022-08-06 15:25:27 +0400

    Added About us section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'homesection': "url('/assets/bg-home-section.png')"
+        'homesection': "url('/assets/bg-home-section.png')",
+        'member_1': "url('/assets/member_1.png')",
+        'member_2': "url('/assets/member_2.png')",
+        'member_3': "url('/assets/member_3.png')",
+        'member_4': "url('/assets/member_4.png')",
+        'next': "url('/assets/next_icon.png')",
+        'prev': "url('/assets/prev_icon.png')",
       }
     },
     fontFamily: {
